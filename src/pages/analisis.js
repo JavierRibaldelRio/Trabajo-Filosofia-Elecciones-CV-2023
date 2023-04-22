fc4fc5cc81e7f9ffbf5731f0cfe3e8aa096dd9c7
JavierRibaldelRio <javierribal@gmail.com>
2023-04-22 21:32:18 +0200

    Creación de Páginas

---
+++ src/pages/analisis.js
@@ -0,0 +1,8 @@
+// Página de analisis correspondiente /analisis
+
+function Analisis() {
+
+    return <><h1>Hola soc analisis pàgina</h1></>
+}
+
+export default Analisis;
---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+// Página principal correspondiente a /
+
+function Main() {
+
+    return <><h1>Hola soc main pàgina</h1>
+
+        <button className="btn btn-primary">asd</button>
+
+    </>
+}
+
+export default Main;
