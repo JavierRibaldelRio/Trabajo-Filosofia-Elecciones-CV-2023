fa5b86cf8ed98a3a0715f9b1e913cbb1b2a7403c
JavierRibaldelRio <javierribal@gmail.com>
2023-04-22 22:37:52 +0200

    Creación del componente word cloud

---
+++ src/components/d3/WordCloud.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import * as d3 from "d3";
+import cloud from "d3-cloud";
+
+import * as Id from "../../json/"
+
+
+class WordCloud extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    // Component did Mount
+
+    componentDidMount() {
+
+        this.crearWordCloud();
+    }
+
+
+    // Crea la nube de palabras
+    crearWordCloud() {
+
+    }
+
+
+
+    render() {
+        return (<>hsdf</>);
+    }
+}
+
+export default WordCloud;
--- src/pages/analisis.js
+++ src/pages/analisis.js
@@ -1,8 +1,14 @@
 // Página de analisis correspondiente /analisis
 
+import WordCloud from "../components/d3/WordCloud";
+
 function Analisis() {
 
-    return <><h1>Hola soc analisis pàgina</h1></>
+    return <><h1>Hola soc analisis pàgina</h1>
+
+        <WordCloud />
+
+    </>
 }
 
 export default Analisis;
