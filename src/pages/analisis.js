197c4bf030aee9eb526fabeb5532d41f62d890f9
JavierRibaldelRio <javierribal@gmail.com>
2023-04-30 21:48:48 +0200

    Elección de un Programa Aleatoriamente

--- src/pages/analisis.js
+++ src/pages/analisis.js
@@ -2,13 +2,18 @@
 
 import NubePalabras from "../components/WordCloud";
 
+import { useState } from "react";
+
 
 
 function Analisis() {
 
+    // Crea el estado que almacena los programas que ha de mostrar y elige uno aleatoriamente para mostar 
+    const [programas, setProgramas] = useState([Math.floor(Math.random() * 100 % 6)]);
 
-    return < ><h1>Hola soc analisis pàgina</h1>
-        <NubePalabras />
+
+    return <><h1>Hola soc analisis pàgina</h1>
+        <NubePalabras programas={programas} />
 
     </>
 }
