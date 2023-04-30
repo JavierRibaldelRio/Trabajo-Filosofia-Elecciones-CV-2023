0db1f88993908c731e2843e4ae57f79932c4c70b
JavierRibaldelRio <javierribal@gmail.com>
2023-04-30 21:23:07 +0200

    Función que crea un array para la nube juntando distintos programas

--- src/scripts/obtenerPartidos.js
+++ src/scripts/obtenerPartidos.js
@@ -2,32 +2,74 @@
  * Este Script se dedica a obtener los JSONS d elos partidos y los formatea
  */
 
+import crearArray from './crearArrayPrograma.js';
 
-import pasarAMayusPalabra from './pasarAMayusPalabra'
 
-const vacias = require('../json/PalabrasVacias.json');
+//Alamacena el tamañ máximo de la letra en la nube
 
 const TAMANYO_MAXIMO_LETRA_NUBE = 48;
 
-// Devuelve un array Listo para la nube con este formato [palabra, tamaño letra , rep palabra en texto, nº palbras en texto, nombre partido, color]
-function crearArray(partido) {
 
-    // Obtiene datos del partido
-    const { nombrePartido, totalWords } = partido;
-    const color = partido.hexPartido;
 
-    // Obtiene el desglose de palabras y elimina las vacias
-    let palabras = partido.count.filter(w => !vacias.includes(w.word));
+// Almacena el nombre de los archvos JSOn de los partidos
+const nombresPartido = ['1984', "Camino", "Cripta", "Peekay", "Idhun", "Trafalgar"];
 
-    // Obtiene el multiplicante de la proporción del tamaño
-    const factor = TAMANYO_MAXIMO_LETRA_NUBE / palabras[0].count;
 
-    return palabras.splice(0, 75).map(x => [pasarAMayusPalabra(x.word), Math.round(x.count * factor), { repeticiones: x.count, totalWords, nombrePartido, color }])
+// Crear un array con todo el desglose de palabras para la nueve
 
+const contenidoNubePartidos = nombresPartido.map(arch => crearArray(require(`../json/programas/${arch}.json`)));
+
+// Devuelve un array con el contenido de la nube en función de los programas que hay que mostrar
+function obtenerContenidoNube(programas) {
+
+    // Si sólo hace falta uno devolvemos el que toque
+    if (programas.length === 1) {
+
+        return contenidoNubePartidos[programas[0]]
+    }
+
+
+    // Si no fusionamos
+    else {
+
+        // Calcula el número de palabras relativas de cada programa  
+        const calcularPalabrasRelativas = (w) => w[2].repeticiones / w[2].totalWords
+
+        let arrayFusion = [];
+
+        for (let i = 0; i < programas.length; i++) {
+
+            arrayFusion.push(...contenidoNubePartidos[programas[i]]);
+        }
+
+
+        // Ordena el array por repeticiones de palabras relativa
+        arrayFusion = arrayFusion.sort((w1, w2) =>
+
+            calcularPalabrasRelativas(w1) < calcularPalabrasRelativas(w2)
+        )
+
+        // Obtiene el factor relativo
+        const factor = (TAMANYO_MAXIMO_LETRA_NUBE - 15) / calcularPalabrasRelativas(arrayFusion[0])
+
+        console.log('factor :>> ', factor);
+
+
+        return arrayFusion.splice(0, 75).map((w) => {
+
+            w[1] = Math.round(factor * w[2].repeticiones / w[2].totalWords)
+
+            return w
+        });
+
+    }
 
 
 }
 
-const arrTorres = crearArray(require('../json/partidos/torre.json'));
 
-export default arrTorres;
+
+export default obtenerContenidoNube;
+
+
+export { TAMANYO_MAXIMO_LETRA_NUBE };
