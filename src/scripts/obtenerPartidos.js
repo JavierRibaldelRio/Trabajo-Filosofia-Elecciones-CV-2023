ad6bfa73d1b054dab61c4b6365eb33a854cbec0f
JavierRibaldelRio <javierribal@gmail.com>
2023-05-02 00:02:56 +0200

    Función que obtinelas 10 palabras mas repetidas entre todos los programas

--- src/scripts/obtenerPartidos.js
+++ src/scripts/obtenerPartidos.js
@@ -35,13 +35,8 @@ function obtenerContenidoNube(programas) {
         // Calcula el número de palabras relativas de cada programa  
         const calcularPalabrasRelativas = (w) => w[2].repeticiones / w[2].totalWords
 
-        let arrayFusion = [];
-
-        for (let i = 0; i < programas.length; i++) {
-
-            arrayFusion.push(...contenidoNubePartidos[programas[i]]);
-        }
-
+        // Une el contenido de los JSON se lecccionados
+        let arrayFusion = unirJSON(programas);
 
         // Ordena el array por repeticiones de palabras relativa
         arrayFusion = arrayFusion.sort((w1, w2) =>
@@ -65,9 +60,117 @@ function obtenerContenidoNube(programas) {
 
 }
 
+// Obtiene el contenido de la gráfica
+
+function obtenerContenidoGrafica(programas) {
+
+    // Si hay más de un programa busca las uniones de los paertidos
+    if (programas.length > 1) {
+
+        // Carga las palabras en el array
+        const palabrasProgramas = unirJSON(programas).sort((w1, w2) => {
+
+            if (w1[0] > w2[0]) { return 1; }
+            else if (w1[0] < w2[0]) { return -1; }
+            return 0;
+        });
+
+        console.log('palabrasProgramas :>> ', palabrasProgramas);
+
+        const unionProgramas = buscarUnirComunes(palabrasProgramas, programas.length - 1, [])
+
+        console.log('palabrasProgramas :>> ', unionProgramas);
+
+        // Ordena las palabras por repetidas comunes > mas veces repetidas 
+        const palabrasSeleccionadas = unionProgramas.sort((w1, w2) => {
+
+            if (w1[1].length < w2[1].length) { return 1 }
+            else if (w1[1].length > w2[1].length) { return -1 }
+            else {
+
+                //Calcula cuaal tien ela palabra mas repetida
+
+                const sumaRelativa = (sum, word) => word.repeticiones / word.totalWords + sum;
+
+                const sumaRw1 = w1[1].reduce(sumaRelativa, 0);
+
+                const sumaRw2 = w2[1].reduce(sumaRelativa, 0);
+
+                return sumaRw2 - sumaRw1;
+
+            }
+
+
+        }).splice(0, 10);
+
+        console.log('palabrasSeleccionadas :>> ', palabrasSeleccionadas);
+
+    }
+
+
+}
+
+
+// Busca las palabras en el array y las une formando el array con toda la combinacion
+
+
+function buscarUnirComunes(programas, posi, resultado = []) {
+
+    // Si ya no queda nada más que mirar
+
+    if (programas.length === 0) {
+
+        return resultado;
+    }
+
+    // Elige la primera palabra del array
+    const word = programas[0][0];
+
+    // Almacena el último índice donde la palabra es la misma ya que el indice no ha cambiado
+
+    let pos = posi;
+
+    while (word !== programas[pos][0]) {
+        pos--;
+    }
+
+    // Si está repetida más de una vez la añade al aray de las repetidas
+    if (pos !== 0) {
+
+        const newItem = [word, [programas[0][2]]]
+
+        for (let i = 0; i < pos; i++) {
+            newItem[1].push(programas[i][2]);
+        }
+
+        resultado.push(newItem);
+    }
+
+    return buscarUnirComunes(programas.slice(pos + 1), posi, resultado);
+
+
+
+
+}
+
+
+
+// Junta todos los jsons seleccionados
+
+function unirJSON(programas) {
+
+    let arrayFusion = [];
+
+    for (let i = 0; i < programas.length; i++) {
+
+        arrayFusion.push(...contenidoNubePartidos[programas[i]]);
+    }
+
+    return arrayFusion
+}
 
 
 export default obtenerContenidoNube;
 
 
-export { TAMANYO_MAXIMO_LETRA_NUBE, nombresPartido };
+export { TAMANYO_MAXIMO_LETRA_NUBE, nombresPartido, obtenerContenidoGrafica };
