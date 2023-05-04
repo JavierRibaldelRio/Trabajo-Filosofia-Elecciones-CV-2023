a32369ee7f9b9675f5ae3b64e0eba2b0d70e0c4b
JavierRibaldelRio <javierribal@gmail.com>
2023-05-04 23:43:55 +0200

    Función que hace el calculo relativo

---
+++ src/scripts/calcularPalabrasRelativas.js
@@ -0,0 +1,4 @@
+// Calcula el número de palabras relativas de cada programa  
+const calcularPalabrasRelativas = (w) => w[2].repeticiones / w[2].totalWords
+
+export default calcularPalabrasRelativas;
