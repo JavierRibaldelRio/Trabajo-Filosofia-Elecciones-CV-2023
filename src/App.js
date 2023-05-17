6507c5b8ec8f29174b4dbe3128fa3e78defdcbf6
JavierRibaldelRio <javierribal@gmail.com>
2023-05-17 19:31:31 +0200

    Classe en vez de componente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
+import React, { Component } from 'react';
+
+
 import Cabecera from './components/Cabecera';
 
 // Subpáginas
@@ -7,29 +10,32 @@ import Analisis from './pages/analisis';
 import PalabrasEliminadas from './pages/palabras-eliminadas';
 import Introduccion from './pages/introduccion';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App" >
 
 
-      <BrowserRouter>
+        <BrowserRouter>
 
-        <Cabecera />
+          <Cabecera />
 
-        <Routes>
+          <Routes>
 
-          <Route path='/' element={<Analisis />} />
-          <Route path='/palabras-eliminadas' element={<PalabrasEliminadas />} />
-          <Route path='/introduccion' element={<Introduccion />} />T
+            <Route path='/' element={<Analisis />} />
+            <Route path='/palabras-eliminadas' element={<PalabrasEliminadas />} />
+            <Route path='/introduccion' element={<Introduccion />} />T
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
 
-      {/* <PiePagina /> */}
+        {/* <PiePagina /> */}
 
 
-    </div>
-  );
+      </div>)
+  }
 }
+
 
 export default App;
